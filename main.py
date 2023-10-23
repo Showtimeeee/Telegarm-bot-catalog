ad43c8260122028f47b9dbd4a2e974c584ddaa86
Showtimeeeeexitgit config --global user.email sipovichit@yandex.rugit config --global user.name Showtimeeee <sipovichit@yandex.ru>
2023-10-23 13:45:58 +0300

    sqlalchemy engine:BEGIN

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from aiogram import Bot, Dispatcher
 import sys
 import logging
 import asyncio
+from aiogram import Bot, Dispatcher
 from app.handlers import router
+from app.database.models import async_main
 from config import TOKEN
 
 
 async def main():
+    await async_main()
+
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
     dp.include_router(router)
+
     await dp.start_polling(bot)
 
 
