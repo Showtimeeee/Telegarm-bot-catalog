24537cedd737b0642449effca1bf474a6328b324
Showtimeeeeexitgit config --global user.email sipovichit@yandex.rugit config --global user.name Showtimeeee <sipovichit@yandex.ru>
2023-10-24 12:10:10 +0300

    keyboard, handlers

--- app/handlers.py
+++ app/handlers.py
@@ -1,11 +1,22 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import CommandStart
-
+import app.keyboards as kb
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer('Привет!')
+    await message.answer('Привет!', reply_markup=kb.main)
+
+
+@router.message(F.text == 'Каталог')
+async def catalog(message: Message):
+    await message.answer('Сделайте свой выбор из каталога', reply_markup=await kb.categories)
+
+
+@router.callback_query(F.text.startswith('category_'))
+async def category_selected(message: Message):
+    category_id = message.data.split('_')[1]
+    await message.answer(f'Вы выбрали категорию {category_id}')
